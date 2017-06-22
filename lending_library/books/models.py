c1690652c8232e5b7190604d0e7e65d93cced817
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-06-22 10:49:12 -0700

    Creating the media root directory

--- lending_library/books/models.py
+++ lending_library/books/models.py
@@ -8,6 +8,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     isbn = models.UUIDField(default=uuid.uuid4, editable=False)
+    cover = models.ImageField(upload_to='book_covers')
 
     def __repr__(self):
         return "<Book: {}>".format(self.title)
