260eef32e0216b406395ca69b5edc0c9a81ba698
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2017-06-22 13:42:27 -0700

    Updating with day 4's material

--- lending_library/books/admin.py
+++ lending_library/books/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from books.models import Book
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'author', 'isbn')
+    list_filter = ('title', )
+
+
+admin.site.register(Book, BookAdmin)
